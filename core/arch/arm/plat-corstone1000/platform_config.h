OP-TEE/optee_os

plat-corstone1000: add corstone1000 platform
These changes are to add corstone1000 platform to optee
core.
arch/arm/plat-vexpress is taken as a reference to make
these changes.

Signed-off-by: Vishnu Banavath <vishnu.banavath@arm.com>

core/arch/arm/plat-corstone1000/platform_config.h
@@ -1,0 +1,36 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022, Arm Limited
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		64
+
+#define GIC_BASE		0x1c000000
+
+#define UART0_BASE		0x1a510000
+#define UART1_BASE		0x1a520000
+#define CONSOLE_UART_BASE	UART1_BASE
+
+#define DRAM0_BASE		0x80000000
+#define DRAM0_SIZE		0x7f000000
+
+#define GICD_OFFSET		0x1000
+#define GICC_OFFSET		0x2000
+
+#define GICD_BASE		(GIC_BASE + GICD_OFFSET)
+#define GICC_BASE		(GIC_BASE + GICC_OFFSET)
+
+#define UART_BAUDRATE		115200
+#define CONSOLE_BAUDRATE	UART_BAUDRATE
+
+#define SYS_COUNTER_FREQ_IN_TICKS	UL(50000000) /* 50MHz */
+
+#define CONSOLE_UART_CLK_IN_HZ	UL(50000000) /* 50MHz*/
+
+#endif /*PLATFORM_CONFIG_H*/

vishnu-banavath/optee_os_plat_corstone1000

Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

core/arch/arm/plat-corstone1000/platform_config.h
@@ -1,0 +1,39 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2022, Arm Limited
+ */
+
+#ifndef PLATFORM_CONFIG_H
+#define PLATFORM_CONFIG_H
+
+#include <mm/generic_ram_layout.h>
+
+/* Make stacks aligned to data cache line length */
+#define STACK_ALIGNMENT		64
+
+#define GIC_BASE		0x1c000000
+
+#define UART0_BASE		0x1a510000
+#define UART1_BASE		0x1a520000
+#define CONSOLE_UART_BASE	UART1_BASE
+
+#define DRAM0_BASE		0x80000000
+#define DRAM0_SIZE		CFG_DDR_SIZE
+
+#define GICD_OFFSET		0x10000
+#define GICC_OFFSET		0x2F000
+
+#define GICD_BASE		(GIC_BASE + GICD_OFFSET)
+#define GICC_BASE		(GIC_BASE + GICC_OFFSET)
+
+#define MM_COMM_BUF_BASE	0x02000000
+#define MM_COMM_BUF_SIZE	0x1000
+
+#define UART_BAUDRATE		115200
+#define CONSOLE_BAUDRATE	UART_BAUDRATE
+
+#define SYS_COUNTER_FREQ_IN_TICKS	UL(50000000) /* 50MHz */
+
+#define CONSOLE_UART_CLK_IN_HZ	UL(50000000) /* 50MHz*/
+
+#endif /*PLATFORM_CONFIG_H*/